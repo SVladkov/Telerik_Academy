22ecc337aa0b43ca64bdcb090827f3b0ae0074bf
SVladkov <s.v.vladkov@gmail.com>
2014-09-08 00:31:20 +0300

    XML parser added

--- Databases/Bookstore/Bookstore.Importer/Program.cs
+++ Databases/Bookstore/Bookstore.Importer/Program.cs
@@ -3,18 +3,103 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Xml.Linq;
 
 using Bookstore.Data;
+using Bookstore.Models;
 
 namespace Bookstore.Importer
 {
-    class Program
+    public class Program
     {
-        static void Main(string[] args)
-        {
-            var db = new BookstoreDbContext();
+        private static BookstoreDbContext db;
 
-            db.Authors.Any();
+        public static void Main(string[] args)
+        {
+            db = new BookstoreDbContext();
+
+            var xmlBooks = XElement.Load(@"..\..\..\complex-books.xml").Elements();
+            
+            foreach (var xmlBook in xmlBooks)
+            {
+                var currentBook = new Book();
+                currentBook.Title = xmlBook.Element("title").Value;
+
+                var isbn = xmlBook.Element("isbn");
+                if (isbn != null)
+                {
+                    var bookExists = db.Books.Any(b => b.ISBN == isbn.Value);
+                    if (bookExists)
+                    {
+                        throw new ArgumentException("ISBN already exists");
+                    }
+
+                    currentBook.ISBN = isbn.Value;
+                }
+
+                var price = xmlBook.Element("price");
+                if (price != null)
+                {
+                    currentBook.Price = decimal.Parse(price.Value);
+                }
+
+                var webSite = xmlBook.Element("web-site");
+                if (webSite != null)
+                {
+                    currentBook.WebSite = webSite.Value;
+                }
+
+                var xmlAuthors = xmlBook.Element("authors");
+                if (xmlAuthors != null)
+                {
+                    foreach (var xmlAuthor in xmlAuthors.Elements("author"))
+                    {
+                        var authorName = xmlAuthor.Value;
+                        
+                        currentBook.Authors.Add(GetAuthor(authorName));
+                    }
+                }
+
+                var xmlReviews = xmlBook.Element("reviews");
+                if (xmlReviews != null)
+                {
+                    foreach (var xmlReview in xmlReviews.Elements("review"))
+                    {
+                        var reviewDate = xmlReview.Attribute("date");
+                        var authorName = xmlReview.Attribute("author");
+
+                        var review = new Review
+                        {
+                            Content = xmlReview.Value,
+                            CreatedOn = reviewDate == null ? DateTime.Now : DateTime.Parse(reviewDate.Value),
+                        };
+
+                        if (authorName != null)
+                        {
+                            review.Author = GetAuthor(authorName.Value);
+                        }
+
+                        currentBook.Reviews.Add(review);
+                    }
+                }
+
+                db.Books.Add(currentBook);
+                db.SaveChanges();
+            }
+        }
+
+        public static Author GetAuthor(string authorName)
+        {
+            Author author = db.Authors.FirstOrDefault(a => a.Name == authorName);
+            if (author == null)
+            {
+                author = new Author
+                {
+                    Name = authorName
+                };
+            }
+
+            return author;
         }
     }
 }
