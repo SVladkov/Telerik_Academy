b1a24805fd77255036778c393726fdf283d51908
SVladkov <s.v.vladkov@gmail.com>
2014-09-07 15:42:00 +0300

    Database course added

---
+++ Databases/ToysStore/ToysStore.SampleDataGenerator/RandomDataGenerator.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ToysStore.SampleDataGenerator
+{
+    internal class RandomDataGenerator : IRandomDataGenerator
+    {
+        private static IRandomDataGenerator randomDataGenerator;
+
+        private Random random;
+
+        private RandomDataGenerator()
+        {
+            this.random = new Random();
+        }
+
+        public static IRandomDataGenerator Instance
+        {
+            get
+            {
+                if (randomDataGenerator == null)
+                {
+                    randomDataGenerator = new RandomDataGenerator();
+                }
+
+                return randomDataGenerator;
+            }
+        }
+
+        public int GetRandomNumber(int min, int max)
+        {
+            throw new NotImplementedException();
+        }
+
+        public string GetRandomString(int length)
+        {
+            throw new NotImplementedException();
+        }
+
+        public string GetRandomStringWithRandomLength(int min, int max)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
